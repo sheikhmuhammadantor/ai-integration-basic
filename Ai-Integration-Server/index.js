82c630d2c1287fbb50047d1ffacb7fc72188dde2
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-02-02 16:29:28 +0600

    Keep Client And Serve In One Repo.

---
+++ Ai-Integration-Server/index.js
@@ -0,0 +1,48 @@
+require('dotenv').config({ path: '.env.local' });
+const express = require('express')
+const cors = require('cors')
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const morgan = require('morgan');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(`${process.env.GEMINI_API_KEY}`);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const port = process.env.PORT || 3000
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nw7sq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// })
+
+async function run() {
+  try {
+    app.get('/', (req, res) => {
+      res.send('Hello from AI Integration Server..')
+    });
+
+    // Ai test get;
+    app.get('/ai-test', async (req, res) => {
+      const prompt = "Explain how AI works";
+      const result = await model.generateContent(prompt);
+      res.send({response : result.response.text()})
+    })
+
+  } finally {
+
+  }
+}
+run().catch(console.dir)
+
+app.listen(port, () => {
+  console.log(`AI is running on port ${port}`)
+})
